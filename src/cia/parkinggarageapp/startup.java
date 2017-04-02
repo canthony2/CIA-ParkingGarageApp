3818451e1a357d6cf8e60e8e116cb0b18f1df82f
knyghtspup <knyghtspup@BrokenWindows>
2017-04-02 12:25:26 -0500

    First Commit

---
+++ src/cia/parkinggarageapp/startup.java
@@ -0,0 +1,19 @@
+package cia.parkinggarageapp;
+
+/**
+ *
+ * @author knyghtspup
+ */
+public class startup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+    }
+    
+}
