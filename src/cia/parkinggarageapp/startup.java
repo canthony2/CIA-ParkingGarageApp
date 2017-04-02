85ceef55b85ca066b5c35cb77bb0f98b02ae06de
knyghtspup <knyghtspup@BrokenWindows>
2017-04-02 12:29:57 -0500

    Inputed process in comments

--- src/cia/parkinggarageapp/startup.java
+++ src/cia/parkinggarageapp/startup.java
@@ -12,6 +12,25 @@ public class startup {
     public static void main(String[] args) {
         // TODO code application logic here
         
+//        Process
+//	Start of Process
+//	Check-In
+//	Vehicle drive up to automated check-in machine
+//	Automated check-in machine issues ticket
+//	Indicates time and date of check-in
+//	Kept in vehicle until check-out
+//	Check-Out
+//	Vehicles drives up to automated check-out machine (if there is one)
+//	Insert ticket received when checking-in
+//	Automated machine reads time and date of check-in
+//	Using special formula
+//	Calculates time spent in garage (no more than 24 hours)
+//	Calculates fee owed
+//	Displays time spent and money owed on electronic display
+//	Payment provided in cash or credit
+//	Automated machine prints receipt
+//	End of Process
+
         
         
     }
