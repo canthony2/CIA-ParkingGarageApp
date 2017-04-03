f4d87ab105c209af2ae046dfb60a24acb4e485ac
knyghtspup <knyghtspup@BrokenWindows>
2017-04-03 10:28:51 -0500

    Created methods in their respective classes

--- src/cia/parkinggarageapp/CheckInTerminal.java
+++ src/cia/parkinggarageapp/CheckInTerminal.java
@@ -10,6 +10,18 @@ package cia.parkinggarageapp;
  * @author knyghtspup
  */
 public class CheckInTerminal {
+
+    void newCheckIn() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void generateTicket() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void endCheckIn() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
     
--- src/cia/parkinggarageapp/CheckOutTerminal.java
+++ src/cia/parkinggarageapp/CheckOutTerminal.java
@@ -10,6 +10,30 @@ package cia.parkinggarageapp;
  * @author knyghtspup
  */
 public class CheckOutTerminal {
+
+    void newCheckOut() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void readTicket() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void displayCustomerInformation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void printReceipt() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void displayBusinessInformation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void endCheckOut() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
     
