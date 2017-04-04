631b17fa5bb1f75ac84136ac06a29f5b4de1790e
knyghtspup <knyghtspup@BrokenWindows>
2017-04-04 09:33:18 -0500

    Created my two types of fee payment

--- src/cia/parkinggarageapp/MinMaxFee.java
+++ src/cia/parkinggarageapp/MinMaxFee.java
@@ -1,19 +1,16 @@
-package cia.parkinggarageapp;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package cia.parkinggarageapp;
 
 /**
  *
  * @author knyghtspup
  */
-public interface FeeCalculatorStrategy {
+public class MinMaxFee {
     
-    public abstract MinMaxFee MinMaxFee();
     
-    public abstract MinNoMaxFee MinNoMaxFee();
     
 }
