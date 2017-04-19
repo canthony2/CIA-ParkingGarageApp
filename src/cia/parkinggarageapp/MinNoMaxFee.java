553cfb64423cfb04a5e8f88f433bcf8ad6747978
knyghtspup <knyghtspup@BrokenWindows>
2017-04-19 18:37:40 -0500

    Working on calculations

--- src/cia/parkinggarageapp/MinNoMaxFee.java
+++ src/cia/parkinggarageapp/MinNoMaxFee.java
@@ -9,7 +9,7 @@ package cia.parkinggarageapp;
  *
  * @author canthony2
  */
-public class MinNoMaxFee implements FeeCalculatorStrategy{
+public class MinNoMaxFee implements FeeCalculatorStrategy {
     
     
     
