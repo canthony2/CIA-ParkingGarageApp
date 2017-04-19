be64861a010f7dd697a2f185e99e2ad33c912770
canthony2 <canthony2@L11714042777.wctc.msft>
2017-04-19 15:44:48 -0500

    created calculator

---
+++ src/cia/parkinggarageapp/Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cia.parkinggarageapp;
+
+/**
+ *
+ * @author canthony2
+ */
+public class Calculator implements FeeCalculatorStrategy {
+    
+    
+    
+}
